c0051c342e86e4123fed34085a734a628bc0f83d
Joe Zaba <joseph.zaba@outlook.com>
2020-11-28 10:11:56 -0700

    Added to Java Spring Boot project.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/main/js/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'src/main/resources/static'),
     library: 'todoLib',
   },
   optimization: {
