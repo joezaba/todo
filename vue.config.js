514b2d5cec9ae0a10bf8d11512aab6f8dcf723a9
Joe Zaba <joseph.zaba@outlook.com>
2020-11-30 20:49:11 -0700

    installed and Configured Vue

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  outputDir: 'src/main/resources/static',
+  pages: {
+    app: {
+      entry: 'src/main/vue/main.js',
+      template: 'src/main/vue/index.html',
+      filename: 'index.html'
+    },
+  },
+}
